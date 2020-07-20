d5e3230306888a6e1ebcf8f53a2d441286e02f66
Waverley Leung <waverley.nyc@gmail.com>
2020-07-20 22:13:53 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,10 +6,8 @@ def join_nested_strings(src)
   while row_index < src.count do
     element_index = 0
     while element_index < src[row_index].count do
-      if src[row_index][element_index].class == String
-        single_value += src[row_index][element_index]
-      end
-      element_index += 1
+        single_value += src[row_index][element_index
+        element_index += 1
     end
     row_index += 1
   end
